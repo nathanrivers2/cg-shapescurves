d6c824048401437824b09ec507691294bd1c62bf
nathanrivers <rive9693@stthomas.edu>
2022-02-24 22:27:59 -0600

    got rectangle done, need to work on math on circle

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -44,13 +44,21 @@ class Renderer {
     }
 
     // ctx:          canvas context
-    drawSlide0(ctx) {
-        
+    drawSlide0(ctx) { //rectangle slide
+        let color = [250, 0, 0, 250];
+        let left_bot = {x:200, y:200}; //x:sets then x elem of the object to the following, same for y:
+        let right_top = {x:600, y:400};
+        this.drawRectangle(left_bot, right_top, color, ctx);
     }
 
     // ctx:          canvas context
     drawSlide1(ctx) {
 
+        //lets make an octagon first 
+        let center = {x:300, y:300}; 
+        let radius = 200; 
+        let color = [250, 0, 0, 250];
+        this.drawCircle(center, radius, color, ctx);
     }
 
     // ctx:          canvas context
@@ -68,7 +76,15 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawRectangle(left_bottom, right_top, color, ctx) {
-        
+
+        let leftTop = {x:left_bottom.x, y:right_top.y}; 
+        let rightBottom = {x:right_top.x, y:left_bottom.y}; // these lines get the opposite of the inputs to create all 4 point we need
+
+        //drawing the lines in the rectangle, should probably edit to have a standard way left to right for example
+        this.drawLine(leftTop, left_bottom, color, ctx); 
+        this.drawLine(rightBottom, right_top, color, ctx);
+        this.drawLine(rightBottom,left_bottom, color, ctx); 
+        this.drawLine(right_top, leftTop, color, ctx);
     }
 
     // center:       object ({x: __, y: __})
@@ -76,7 +92,31 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawCircle(center, radius, color, ctx) {
+        //x=center(x)+radius*cos(angle) 
+        //y=center(y)+radius*sin(angle) 
+
+
+        let edge = {x:center.x, y:center.y+radius};
+        this.drawLine(center,edge,[0,250,0,250],ctx);
+
+        let i=0;
         
+        // this is jumping way too many degrees, need to check the math out.
+        for(let x=0; x<10; x++){ 
+            let x = center.x+radius*Math.cos(i); 
+            let y = center.y+radius*Math.sin(i); 
+
+            i=i+10
+
+            let x1 = center.x+radius*Math.cos(i);
+            let y1 = center.y+radius*Math.sin(i); 
+
+            let p0 ={x:x, y:y}; 
+            let p1 = {x:x1, y:y1};
+        
+            this.drawLine(p0,p1,color,ctx);
+
+        }
     }
 
     // pt0:          object ({x: __, y: __})
